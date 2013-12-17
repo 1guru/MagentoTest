5c8548cb52a8a0f0af1457c3393c5568069ffab1
Ionut Caval <ionut.caval@gmail.com>
2013-12-17 08:20:24 +0200

    com s

--- scripts/sitemap_read.php
+++ scripts/sitemap_read.php
@@ -1,12 +1,14 @@
 <?php
 
 $file_content = file_get_contents(dirname(__FILE__) . '/../sitemap/sitemap.xml');
-$matches=null;
+$matches = null;
 preg_match_all('/(http:[^<]*)/', $file_content, $matches);
 
-if(!empty($matches[1])) {
+if (!empty($matches[1])) {
 	foreach ($matches[1] as $link) {
-		file_get_contents($link);
-		echo "$link\n";
+		if (strstr($link, 'http://www. sitemaps.org/schemas/sitemap/') !== FALSE) {
+			file_get_contents($link);
+			echo "$link\n";
+		}
 	}
 }
